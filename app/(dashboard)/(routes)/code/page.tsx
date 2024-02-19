42d241e2118a9d08c8198d39999daedd46376d3c
filway <filway@126.com>
2024-02-19 23:02:42 +0800

    Image Generation AI(Open AI)

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -100,7 +100,7 @@ const CodePage: NextPage = () => {
               <Loader />
             </div>
           )}
-          {messages.length === 0 && !isLoading && <Empty label="No conversation started." />}
+          {messages.length === 0 && !isLoading && <Empty label="No code generated." />}
           <div className="flex flex-col-reverse gap-y-4">
             {messages.map((message) => (
               <div
